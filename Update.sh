4af90259afc293207ae7126875a245577718ebbc
Unknown <82444721+LuckyJMax@users.noreply.github.com>
2021-08-16 12:52:32 +0530

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,6 +1,6 @@
 
 
-rm -r -f (Password-Generator)
+rm -r -f Password-Generator
 git clone https://github.com/LuckyJMax/Password-Generator.git
 echo -e "\033[1;33m    Update Was Successfully >
 echo -e "\033[1;33m    Tool By  LuckyJMax (S>
