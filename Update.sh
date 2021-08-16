0970e575ec15ad02b1a607ac5bacaf9b9a72ddb8
Unknown <82444721+LuckyJMax@users.noreply.github.com>
2021-08-16 12:54:16 +0530

    Delete Update.sh

--- Update.sh
+++
@@ -1,9 +0,0 @@
-
-
-rm -r -f Password-Generator
-git clone https://github.com/LuckyJMax/Password-Generator.git
-echo -e "\033[1;33m    Update Was Successfully >
-echo -e "\033[1;33m    Tool By  LuckyJMax (S>
-sleep 3
-cd Password-Generator
-python PasswordGenerator.py
