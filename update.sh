f4ed6b46a70718325f97cbfb4aa851ea14e62398
Unknown <82444721+LuckyJMax@users.noreply.github.com>
2021-08-16 12:45:13 +0530

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,9 @@
+
+
+rm -rf (Password-Generator)
+git clone https://github.com/LuckyJMax/Password-Generator.git
+echo -e "\033[1;33m    Update Was Successfully >
+echo -e "\033[1;33m    Tool By  LuckyJMax (S>
+sleep 3
+cd Password-Generator
+python PasswordGenerator.py
